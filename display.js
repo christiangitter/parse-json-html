501bfa0828cc42ca24bf93044a59eae84624dc62
Christian Gitter <gitter.christian@yahoo.de>
2021-11-10 19:46:23 +0100

    Display Beer

--- display.js
+++ display.js
@@ -1,4 +1,11 @@
-fetch('https://beerapilive.herokuapp.com/top5beers')
+fetch("https://beer-live.p.rapidapi.com/top10beers", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "beer-live.p.rapidapi.com",
+		"x-rapidapi-key": "6b5438322bmsh499cfcde3ebbe7fp11fabfjsn0a23997d94f6"
+	}
+})
+    
             .then(function (response) {
                 return response.json();
             })
@@ -14,7 +21,7 @@ function appendData(data) {
 var mainContainer = document.getElementById("topbeer");
 for (var i = 0; i < data.length; i++) {
     var div = document.createElement("div");
-     div.innerHTML = 'Top 1: ' + data[i].bier + ' ' + data[i].herkunft;
+     div.innerHTML = 'Top-Bier: ' + data[i].bier + ' ' + data[i].herkunft;
     mainContainer.appendChild(div);
   }
 }
